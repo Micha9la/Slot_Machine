0caf1bf17be87efe6c92462d005e21f7eeddef48
michaela <m.hunarwindhager@gmail.com>
2024-10-03 13:21:13 +0200

    created 3x3 grid and consolidated repetitive code into 2 for loops

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Slot_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the slot machine.");
+            int[,] grid = new int[3, 3];
+            for (int lineIndex = 0; lineIndex < 3; lineIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < 3; columnIndex++)
+                { 
+                    Console.Write(grid[lineIndex, columnIndex] );
+                }
+            }
+
+        }
+    }
+}
