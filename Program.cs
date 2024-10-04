a7217b7868c74be8b90b46fa3dff5fb09718d6ac
michaela <m.hunarwindhager@gmail.com>
2024-10-04 13:10:07 +0200

    nested for loops to display 3x3 grid row by row with a space in between. created constants for random number generator

--- Program.cs
+++ Program.cs
@@ -4,14 +4,25 @@
     {
         static void Main(string[] args)
         {
+            const int UPPER_NUMBER_RANGE = 101;
+            const int LOWER_NUMBER_RANGE = 1;
+            Random random = new Random();
+            int randomNumber = random.Next(LOWER_NUMBER_RANGE, UPPER_NUMBER_RANGE);
+
+
             Console.WriteLine("Welcome to the slot machine.");
             int[,] grid = new int[3, 3];
-            for (int lineIndex = 0; lineIndex < 3; lineIndex++)
+
+            for (int lineIndex = randomNumber; lineIndex < 3; lineIndex++)
             {
-                for (int columnIndex = 0; columnIndex < 3; columnIndex++)
-                { 
-                    Console.Write(grid[lineIndex, columnIndex] ); 
+                for (int columnIndex = randomNumber; columnIndex < 3; columnIndex++)
+                {
+                    Console.Write(grid[lineIndex, columnIndex] + " ");
                 }
+                //this prompt helps to move curser to next line in the console.
+                //After printing all the values in one row of the grid array,
+                //Console.WriteLine() is called, which moves the cursor to the next line.
+                Console.WriteLine();
             }
 
         }
